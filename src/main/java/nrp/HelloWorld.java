29a8bec1c0e1198b0e9d9de2b21a2df34abaee8a
paul <paul.beck.berlin@gmail.com>
2016-12-10 17:08:05 +0100

    added logger to maven, and the homepage

--- src/main/java/nrp/HelloWorld.java
+++ src/main/java/nrp/HelloWorld.java
@@ -2,10 +2,35 @@ package nrp;
 
 import static spark.Spark.*;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class HelloWorld {
+	
+	public static final String HTML_PATH = "src"+File.separator+"main"+File.separator+"resources"+File.separator;
+	
 	public static void main(String[] args) {
-        get("/", (req, res) -> "<html><h1> NIKLAS WAR HIER! </h1> </html>");
+        get("/", (req, res) ->{
+        	
+        	return getHtml("Homepage.html");
+        });
         
-        post("/result",  (req, res) -> "<html><h1> NIKLAS WAR HIER! </h1> </html>");
+        post("/result",  (req, res) -> {
+        	System.out.println(req.params().toString());
+        	return "THIS IS SHIT";
+        });
     }
+	
+	private static String getHtml(String path) throws IOException{
+		return readFile(HTML_PATH + path,  Charset.defaultCharset());
+	}
+	
+	private static String readFile(String path, Charset encoding) throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+	  	return new String(encoded, encoding);
+	}
 }
